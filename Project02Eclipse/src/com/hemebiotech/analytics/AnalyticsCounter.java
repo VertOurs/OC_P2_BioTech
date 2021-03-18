22315461766461b48380ffac9c620f032a61e733
VertOurs <vertour.dev@gmail.com>
2021-03-18 14:34:04 +0100

    mise en forme du code
    séparation en partie +
    questions pour demain

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,18 +1,29 @@
 package com.hemebiotech.analytics;
 
+// es que je peu pas remplacer cela par  java.io.*;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
+	/* -------------------------------------------METHODE DE LECTURE-------------------------------------*/
 
+	// void car je n'attends pas un retour précis mais pourquoi utiliser static dans ce contexte?
+	static void lecture(){
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
+	}
+
+	/*--------------------------------------------METHODE COMPTAGE DES OCCURENCES------------------------*/
+
+	static void compterLesOccurences(){
+		// déplacement des variables au plus près du blocs qui les utilise
+
+		//j'ai enlevé private et static, cela a l'air de mieux marché mais je sais pas pourquoi
+		int headacheCount = 0;
+		int rashCount = 0;
+		int pupilCount = 0;
 
 		int i = 0;
 		int headCount = 0;
@@ -29,15 +40,36 @@ public class AnalyticsCounter {
 			else if (line.contains("pupils")) {
 				pupilCount++;
 			}
-
-			line = reader.readLine();	
+			// a quoi peux bien servir cette ligne ?
+			line = reader.readLine();
 		}
-		
-		// next generate output
+	}
+
+	/* -----------------------------------------METTRE EN ORDRE ALPHA----------------------------------*/
+
+	static void alphabetique(){
+
+
+
+	}
+
+	/*-----------------------------------SORTIR DANS UN FICHIERS RESULT--------------------------------*/
+
+	static void ecriture(){
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+	}
+
+	/*-----------------------------------METHODE MAIN DANS QUI APPEL CHACUNE DES METHODES-------------*/
+
+	public static void main(String args[]) throws Exception {
+		lecture();
+		compterLesOccurences();
+		alphabetique();
+		ecriture();
+
 	}
 }
