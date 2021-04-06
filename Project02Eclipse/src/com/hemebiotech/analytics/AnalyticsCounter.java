e6cbeb885a215b031fae7d241b6345eef3cf3837
VertOurs <vertour.dev@gmail.com>
2021-04-06 12:34:32 +0200

    Modification du projet main+ analytic
    création d'une méthode Main dans un fichier séparé.
    mise en forme de la méthode lecture.
    mise en forme de la méthode compterlesoccurence.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,100 +1,50 @@
 package com.hemebiotech.analytics;
 
-// es que je peu pas remplacer cela par  java.io.*;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.*;
 
 public class AnalyticsCounter {
 	/* -------------------------------------------METHODE DE LECTURE-------------------------------------*/
 
-	// void car je n'attends pas un retour précis mais pourquoi utiliser static dans ce contexte?
-	// dois je fermer cette methode de lecture? a quoi sert le deuxieme ligne.
-	static void lecture(){
+	public List<String> lecture() throws IOException {
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+
+		List<String> listlecture = new ArrayList();
 		String line = reader.readLine();
-	}
 
-	/*--------------------------------------------METHODE COMPTAGE DES OCCURENCES------------------------*/
 
-	static void compterLesOccurences(){
-		// déplacement des variables au plus près du blocs qui les utilise
-
-		//j'ai enlevé private et static, cela a l'air de mieux marché mais je sais pas pourquoi
-		//le compilateur me renvoi des erreur cannot find symbol varaible pk?
-		int headacheCount = 0;
-		int rashCount = 0;
-		int pupilCount = 0;
-
-		int i = 0;
-		int headCount = 0;
-		while (line != null) {
-			i++;
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-			// a quoi peux bien servir cette ligne ?
+		while (line != null){
+			listlecture.add(line);
 			line = reader.readLine();
 		}
+
+		return	listlecture;
 	}
+	/*--------------------------------------------METHODE COMPTAGE DES OCCURENCES------------------------*/
+
+	public int compterLesOccurences(String compteOccurences, List<String> listlecture) {
+
+        int nbOccurence = 0;
+
+        for (int i = 0; i < listlecture.size(); i++) {
+            String a = listlecture.get(i);
+
+            if (a == compteOccurences) {
+                nbOccurence++;
+            }
+        }
+    }
 
 	/* -----------------------------------------METTRE EN ORDRE ALPHA----------------------------------*/
 
-	static void alphabetique(){
-      /* j'ai trouver des exemples de type :
-      Java Program to Sort Names in an Alphabetical Order
-
-import java.io.*;
-
-class GFG {
-    public static void main(String[] args)
-    {
-        // storing input in variable
-        int n = 4;
-
-        // create string array called names
-        String names[]
-            = { "Rahul", "Ajay", "Gourav", "Riya" };
-        String temp;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-
-                // to compare one string with other strings
-                if (names[i].compareTo(names[j]) > 0) {
-                    // swapping
-                    temp = names[i];
-                    names[i] = names[j];
-                    names[j] = temp;
-                }
-            }
-        }
-
-        // print output array
-        System.out.println(
-            "The names in alphabetical order are: ");
-        for (int i = 0; i < n; i++) {
-            System.out.println(names[i]);
-        }
-    }
-}
+	//public void alphabetique(){}
 
 
-	}
 
-     mais pour le coup je comprend pas comment cela marche  */
 
 	/*-----------------------------------SORTIR DANS UN FICHIERS RESULT--------------------------------*/
 
-	static void ecriture(){
+	public void ecriture(){
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
@@ -102,13 +52,7 @@ class GFG {
 		writer.close();
 	}
 
-	/*-----------------------------------METHODE MAIN DANS QUI APPEL CHACUNE DES METHODES-------------*/
 
-	public static void main(String args[]) throws Exception {
-		lecture();
-		compterLesOccurences();
-		alphabetique();
-		ecriture();
 
-	}
+
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,25 @@
+package com.hemebiotech.analytics;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        AnalyticsCounter r = new AnalyticsCounter();
+        List<String> x = r.lecture();
+        
+
+
+
+
+
+     /*   r.compterLesOccurences();
+        r.alphabetique();
+        r.ecriture();
+        */
+    }
+
+
+
+}
